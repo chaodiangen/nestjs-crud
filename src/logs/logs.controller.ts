3bee1be949c2b23b0f577cf56f0af37a85ef350e
chaodiangen <chao.diangen@trs.com.cn>
2023-05-08 21:27:55 +0800

    日志

---
+++ src/logs/logs.controller.ts
@@ -0,0 +1,38 @@
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { Expose } from 'class-transformer';
+import { IsString } from 'class-validator';
+import { Serialize } from 'src/decorators/serialize.decorator';
+import { AdminGuard } from 'src/guards/admin.guard';
+import { JwtGuard } from 'src/guards/jwt.guard';
+// import { SerializeInterceptor } from '../interceptors/serialize.interceptor';
+
+class LogsDto {
+  @IsString()
+  msg: string;
+
+  @IsString()
+  id: string;
+}
+
+class PubLicLogsDto {
+  @Expose()
+  msg: string;
+}
+
+@Controller('logs')
+@UseGuards(JwtGuard, AdminGuard)
+export class LogsController {
+  @Get()
+  getTest() {
+    return 'test';
+  }
+
+  @Post()
+  // 后置拦截器，Expose 隐藏导出字段
+  // @UseInterceptors(new SerializeInterceptor(PubLicLogsDto))
+  @Serialize(PubLicLogsDto)
+  postTest(@Body() dto: LogsDto) {
+    console.log(dto);
+    return dto;
+  }
+}
---
+++ src/logs/logs.module.ts
@@ -0,0 +1,59 @@
+import { Module } from '@nestjs/common';
+import { WinstonModule, WinstonModuleOptions } from 'nest-winston';
+import { ConfigService } from '@nestjs/config';
+import * as winston from 'winston';
+import { Console } from 'winston/lib/winston/transports';
+import { utilities } from 'nest-winston';
+import * as DailyRotateFile from 'winston-daily-rotate-file';
+import { LogEnum } from '../enum/config.enum';
+import { LogsController } from './logs.controller';
+import { LogsService } from './logs.service';
+import { UserModule } from '../user/user.module';
+
+function createDailyRotateTrasnport(level: string, filename: string) {
+  return new DailyRotateFile({
+    level,
+    dirname: 'logs',
+    filename: `${filename}-%DATE%.log`,
+    datePattern: 'YYYY-MM-DD-HH',
+    zippedArchive: true,
+    maxSize: '20m',
+    maxFiles: '14d',
+    format: winston.format.combine(
+      winston.format.timestamp(),
+      winston.format.simple(),
+    ),
+  });
+}
+@Module({
+  imports: [
+    UserModule,
+    WinstonModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const consoleTransports = new Console({
+          level: 'info',
+          format: winston.format.combine(
+            winston.format.timestamp(),
+            utilities.format.nestLike(),
+          ),
+        });
+
+        return {
+          transports: [
+            consoleTransports,
+            ...(configService.get(LogEnum.LOG_ON)
+              ? [
+                  createDailyRotateTrasnport('info', 'application'),
+                  createDailyRotateTrasnport('warn', 'error'),
+                ]
+              : []),
+          ],
+        } as WinstonModuleOptions;
+      },
+    }),
+  ],
+  controllers: [LogsController],
+  providers: [LogsService],
+})
+export class LogsModule {}
